e8e1ac530e9cd775da56b479c9e636fcdffbd030
GMcD <gary.macdonald@projectscapa.com>
2019-12-25 13:07:27 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MyPackageName',
+    version='0.2',
+    url='https://github.com/GMcD/MachineLearning.git',
+    author='Gary MacDonald',
+    author_email='gary.macdonald@projectscapa.com',
+    description='PyTorch Samples',
+    packages=find_packages(),    
+    install_requires=[],
+)
