cb2fc4a673456f4e4379e648cfb0407024b066d1
GMcD <gary.macdonald@projectscapa.com>
2019-12-27 17:00:51 +0000

    Moved VehicleNet to package.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 make package
+git add .
 git commit -m "Moved VehicleNet to package."
+git push origin :refs/tags/v0.6
 git tag -fa v0.6 -m "VehicleNet v6."
 git push
 git push origin v0.6
