ec855062629400d824de87734dbd012a5d08e51c
GMcD <gary.macdonald@projectscapa.com>
2019-12-28 11:06:03 +0000

    Version Bump.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VER=9.1
+VER=9.2
 
 make package
 git add .
-git commit -m "Moved VehicleNet to package."
+git commit -m "Version Bump."
 git push origin :refs/tags/v0.${VER}
 git tag -fa v0.${VER} -m "VehicleNet v${VER}."
 git push
